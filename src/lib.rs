494cc28babfebda41b6edf29247e5ea24d427df5
Ed Reeseg <ed.reeseg@clickbank.com>
2022-10-06 15:28:24 -0500

    Example poem, testing lib.rs changes.

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,29 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string((config.filename))?;
-    println!("With text:\n{}", contents);
+    let contents = fs::read_to_string(config.filename)?;
+    let contents = contents.split("\n");
+    let mut results: Vec<String> = Vec::new();
+    for (i, line) in contents.enumerate() {
+        let mut line = line;
+        let mut offset = 0;
+        while let Some(n) = line.find(&config.query) {
+            results.push(format!(
+                "({}, {})",
+                (i + 1).to_string(),
+                (n + 1 + offset).to_string()
+            ));
+            offset += n + 1;
+            line = &line[n + 1..];
+        }
+    }
+    if results.len() > 0 {
+        println!(
+            "Matches for query found at (line, char): {}",
+            results.join(", ")
+        );
+    } else {
+        println!("No matches found for query.");
+    }
     Ok(())
 }
